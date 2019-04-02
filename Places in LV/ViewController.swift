ed2c18fd04729edee435153dffc4f5f618d47a17
Aleksandrs Muravjovs <kavalskij@gmail.com>
2019-04-02 14:19:09 +0300

    Initial Commit

---
+++ Places in LV/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Places in LV
+//
+//  Created by Aleksandrs Muravjovs on 02/04/2019.
+//  Copyright © 2019 Aleksandrs Muravjovs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
